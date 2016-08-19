f89a58cb35a6463e3267576029bc39ab2b535c24
Matthias Ludwig <mludwig@hausgross.de>
2016-08-19 16:11:40 +0200

    Initial Commit

---
+++ MLAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLAlertController
+//
+//  Created by Matthias Ludwig on 19.08.16.
+//  Copyright © 2016 Matthias Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLAlertController
+//
+//  Created by Matthias Ludwig on 19.08.16.
+//  Copyright © 2016 Matthias Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
